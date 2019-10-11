5e1deecb09e7d45db3dc24c16e359564459c3f4b
Nikola Pavlovic <npavlovic101@gmail.com>
2019-10-11 23:28:24 +0200

    initial commit

---
+++ main.lua
@@ -0,0 +1,26 @@
+local frame = CreateFrame("Frame")
+local hooks = {}
+local CHANNELS = {
+    LOOKING_FOR_GROUP = 'LookingForGroup',
+    WORLD = 'World',
+    GENERAL = 'General'
+}
+
+local filter = "";
+SLASH_CLFG1 = "/clfg"
+SlashCmdList["CLFG"] = function(msg)
+    filter = msg;
+end 
+
+frame:RegisterEvent("CHAT_MSG_CHANNEL")
+frame:SetScript("OnEvent", function(self, event, message, author, c, d, e, f, g, h, channel)
+    if (channel == CHANNELS.LOOKING_FOR_GROUP or channel == CHANNELS.WORLD or channel == CHANNELS.GENERAL) then
+        if (filter ~= "" and string.find(message, filter)) then
+            print("|cFF0000FF|Hplayer:"..author.."|h"..author.."|h", message);
+        end
+    end
+end)
+
+JoinChannelByName(CHANNELS.LOOKING_FOR_GROUP);
+JoinChannelByName(CHANNELS.WORLD);
+JoinChannelByName(CHANNELS.GENERAL);
