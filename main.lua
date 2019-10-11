684ec52580be90bfb590d2ac8a36e11e70391f68
Nikola Pavlovic <npavlovic101@gmail.com>
2019-10-11 23:47:02 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -16,7 +16,7 @@ frame:RegisterEvent("CHAT_MSG_CHANNEL")
 frame:SetScript("OnEvent", function(self, event, message, author, c, d, e, f, g, h, channel)
     if (channel == CHANNELS.LOOKING_FOR_GROUP or channel == CHANNELS.WORLD or channel == CHANNELS.GENERAL) then
         if (filter ~= "" and string.find(message, filter)) then
-            print("|cFF0000FF|Hplayer:"..author.."|h"..author.."|h", message);
+            print("|cFF0EC4DB|Hplayer:"..author.."|h"..author.."|h", message);
         end
     end
 end)
